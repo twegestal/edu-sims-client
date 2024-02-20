818ce5233aa749258c35f4d602961da9ef484fff
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 19:44:28 +0100

    Test different endpoints

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,16 +1,25 @@
-import { Button, Center, Flex, Text, VStack } from "@chakra-ui/react";
+import { Button, Center, Text, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 
 export default function HomePage() {
-  const [serverResponse, setServerResponse] = useState("EDU-SIMS");
+  const [serverResponse, setServerResponse] = useState("");
+  const url = import.meta.env.VITE_API_BASE_URL;
 
-  const fetchData = async () => {
+  const sayHello = async () => {
     try {
-      const url = import.meta.env.VITE_API_BASE_URL;
-      console.log("url", url);
       const res = await fetch(url);
       const hello = await res.json();
       setServerResponse(hello);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const checkHealth = async () => {
+    try {
+      const res = await fetch(`${url}health`);
+      const health = await res.json();
+      setServerResponse(health);
     } catch (error) {
       console.error(error);
     }
@@ -20,7 +29,8 @@ export default function HomePage() {
     <Center w={"100vw"} h={"100vh"}>
       <VStack>
         <Text>{serverResponse}</Text>
-        <Button onClick={fetchData}>CLICK ME!</Button>
+        <Button onClick={sayHello}>Say hello!</Button>
+        <Button onClick={checkHealth}>Server healthy?</Button>
       </VStack>
     </Center>
   );
