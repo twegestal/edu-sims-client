33dfe8637a037b77d7999572836050aa8c089b32
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 10:38:55 +0100

    Update Home.page.jsx

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,7 +1,12 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
 export default function HomePage() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <p>Hello</p>
+      <Text>{count}</Text>
+      <Button onClick={() => setCount(count + 1)}>Click me!</Button>
     </>
   );
 }
