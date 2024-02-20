2fc2f55bc16f6da2e6eb4b610ed532b9621adc92
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 13:33:15 +0100

    test

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,11 +1,6 @@
 import { Text } from "@chakra-ui/react";
-import { useEffect } from "react";
 
 export default function HomePage() {
-  useEffect(() => {
-    console.log("hejejkuken");
-  }, []);
-
   return (
     <>
       <Text>EDU-SIMS</Text>
