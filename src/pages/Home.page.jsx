de909d878b25189a41c83de35fcfb8e5643a2f03
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 16:07:02 +0100

    test api

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,9 +1,24 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [serverResponse, setServerResponse] = useState("EDU-SIMS");
+
+  const fetchData = async () => {
+    console.log("here?");
+    try {
+      const res = await fetch("/api");
+      const hello = await res.json();
+      setServerResponse(hello);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <Text>EDU-SIMS</Text>
+      <Text>{serverResponse}</Text>
+      <Button onClick={fetchData}>CLICK ME!</Button>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build'
+  }, 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://edu-sims-server-prod-1be974bf0374.herokuapp.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
   }
 })
