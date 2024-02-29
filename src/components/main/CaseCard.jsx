749ef2ede89084b1d1498fd5f7d612854a20ff58
LektorVinberg <97341150+LektorVinberg@users.noreply.github.com>
2024-02-29 16:03:02 +0100

    fixed bug after merge
    craxy code in CaseCard.jsx

--- src/components/main/CaseCard.jsx
+++ src/components/main/CaseCard.jsx
@@ -16,34 +16,38 @@ import {
 import stethoscope from '../../assets/images/svg/stethoscope.svg';
 import { useNavigate } from 'react-router-dom';
 
-
-export default function CaseCard() {
+export default function CaseCard({ name, medicalFieldId }) {
   const navigate = useNavigate();
 
-const getColorScheme = (medicalField) =>{
-  
-  switch(medicalField){
-    case '6ff9e368-1e44-46d1-8192-ac3069c34f20': return 'blue.50';
-  }
-}
-
-export default function CaseCard({name, medicalFieldId}) {
+  const getColorScheme = (medicalField) => {
+    switch (medicalField) {
+      case '6ff9e368-1e44-46d1-8192-ac3069c34f20':
+        return 'blue.50';
+    }
+  };
 
   return (
-    <Card bg={getColorScheme(medicalFieldId)} minH={'185px'} minW={'255px'} maxW={'370'} borderWidth={1} borderColor={'gray.400'}>
+    <Card
+      bg={getColorScheme(medicalFieldId)}
+      minH={'185px'}
+      minW={'255px'}
+      maxW={'370'}
+      borderWidth={1}
+      borderColor={'gray.400'}
+    >
       <CardBody>
         <HStack>
           <Image boxSize={'30%'} src={stethoscope} />
           <VStack>
-            <Heading size={'md'} pl={4} >{name}👿</Heading>
-            
+            <Heading size={'md'} pl={4}>
+              {name}👿
+            </Heading>
           </VStack>
         </HStack>
       </CardBody>
       <Divider color={'lightgray'} />
       <CardFooter>
         <HStack spacing={2} justifyContent={'space-between'} w={'full'}>
-
           <Button
             flexShrink={1}
             variant={'solid'}
@@ -53,7 +57,6 @@ export default function CaseCard({name, medicalFieldId}) {
             }}
           >
             Öppna fall
-
           </Button>
           <Box flexShrink={1} minWidth={'30%'}>
             <Text>Avklarat</Text>
