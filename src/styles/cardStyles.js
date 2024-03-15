cfa95e34d1435d552db0532ae1c36f21a6d2c608
LektorVinberg <97341150+LektorVinberg@users.noreply.github.com>
2024-03-15 12:23:03 +0100

    custom card styles added
    Added custom card theming for use in the desktop view when playing a case
    
    Co-Authored-By: Max <97522672+CaptainZoraaaaa@users.noreply.github.com>

---
+++ src/styles/cardStyles.js
@@ -0,0 +1,36 @@
+import { cardAnatomy } from '@chakra-ui/anatomy';
+import { createMultiStyleConfigHelpers } from '@chakra-ui/react';
+
+const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(
+  cardAnatomy.keys,
+);
+
+const edu_case = definePartsStyle({
+  container: {
+    borderColor: 'gray.400',
+    borderWidth: '2px',
+    bgColor: 'cyan.50',
+
+    _dark: {
+      bgColor: 'gray.600',
+    },
+  },
+});
+
+const edu_card = definePartsStyle({
+  container: {
+    borderColor: 'black',
+    borderWidth: '4px',
+    backgroundColor: '#206bA9',
+  },
+});
+
+// export variants in the component theme
+export const cardTheme = defineMultiStyleConfig({
+  variants: {
+    edu_case,
+    edu_card,
+  },
+});
+
+// now we can use the new funky variant
